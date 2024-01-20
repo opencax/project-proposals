1c479806e83d2b2e94ae548325605c2138526b7d
Torsten Paul <Torsten.Paul@gmx.de>
2024-01-20 15:53:45 +0100

    Re-seed display order every minute.

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,16 +1,19 @@
-import type { PageLoad } from './$types';
+import type { PageLoad } from './$types'
+import seedrandom from 'seedrandom'
 
 export const prerender = true;
 
 const shuffle = (arr: Issue[]): Issue[] => {
+    // re-seed every minute
+    const rng = seedrandom(new Date().toISOString().substring(0, 16));
     return arr
-        .map((e) => ({ e, sort: Math.random() }))
+        .map((e) => ({ e, sort: rng() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ e }) => e)
 }
 
 export const load: PageLoad = (async ({ fetch }) => {
-    const res = await fetch('./issues.json');
-    const issues: Issue[] = await res.json();
-    return { issues: shuffle(issues) }
+    const res = await fetch('./issues.json')
+    const issues: Issue[] = shuffle(await res.json())
+    return { issues }
 })
