d68ca5ff72047829d40a13e402e511d06ebceee7
Torsten Paul <Torsten.Paul@gmx.de>
2025-02-09 22:32:42 +0100

    Update fetch-issues.sh for 2025.

--- scripts/fetch-issues.sh
+++ scripts/fetch-issues.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-URL='https://api.github.com/repos/opencax/gsoc/issues?state=open&labels=GSoC+2024&per_page=50'
+URL='https://api.github.com/repos/opencax/gsoc/issues?state=open&labels=GSoC+2025&per_page=50'
 
 curl "$URL" > static/issues.json
